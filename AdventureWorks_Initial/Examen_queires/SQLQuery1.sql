effaf8feb496d5aaf97f38e62d0a36fc28886954
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2025-04-03 01:27:41 +0200

    examen queries

---
+++ AdventureWorks_Initial/Examen_queires/SQLQuery1.sql
@@ -0,0 +1,19 @@
+/*
+Crea una tabla de dos columnas. 
+La primera columna es el nombre del departamento (HumanResources.Department).
+La segunda consulta es la lista, separando cada empleado con una barra verticale "|", 
+del apellido y el nombre (Person.Person), separados por una coma,  de cada empleado del departamento.
+
+La tabla mostrará los datos de los departamentos con menos de 5 empleados.
+
+*/
+
+SELECT 
+    d.Name AS Departamento,
+    STRING_AGG(p.FirstName + ', ' + p.LastName, ' | ') AS Empleados,
+	Count (*) as empleados
+FROM HumanResources.EmployeeDepartmentHistory dh
+JOIN Person.Person p ON dh.BusinessEntityID = p.BusinessEntityID
+JOIN HumanResources.Department d ON dh.DepartmentID = d.DepartmentID
+GROUP BY dh.DepartmentID, d.Name
+HAVING Count (*) <5
